90047678c491cf3617132038a6c39e8bb7c6722b
J_Ghannam <97244554+JGhannam@users.noreply.github.com>
2022-09-28 20:02:48 +0300

    some stars and sky box "kinda"

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -1,14 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useRef, useEffect, useState } from 'react';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { useRef, useEffect, useState } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
-import { useLoading } from '../../lib/loading';
-import useApi from '../../hooks/useApi';
-import issLocation from '../../api/iss-now';
-import calcPosFromLatLonRad from '../../utils/calcPosFromLatLong';
+import { useLoading } from "../../lib/loading";
+import useApi from "../../hooks/useApi";
+import issLocation from "../../api/iss-now";
+import calcPosFromLatLonRad from "../../utils/calcPosFromLatLong";
+import BG from "../../Images/background-1.jpeg";
 
 export default function Main() {
   //Groups
@@ -57,6 +58,37 @@ export default function Main() {
     renderer.setSize(width, height);
     currentRef.appendChild(renderer.domElement);
 
+    //  skybox BG image
+    // // const loader = new THREE.TextureLoader();
+    // // const texture = loader.load(BG);
+    // // scene.background = texture;
+
+    /*
+  Dear team, 
+
+  this is a temp skybox that we can change later 
+  you may say that is looks cool 
+  but if you rotate the camera you will see that it is not a skybox
+  and someones signature is on the image
+
+  I will leave it here for now, but we should change it later
+
+  have a lovely day 
+
+  Ghannam
+  */
+
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load(BG);
+    const skybox = new THREE.Mesh(
+      new THREE.SphereGeometry(80, 100, 100), // radius, widthSegments, heightSegments
+      new THREE.MeshBasicMaterial({
+        map: texture,
+        side: THREE.BackSide,
+      })
+    );
+    scene.add(skybox);
+
     //Interval update position
     const interval = setInterval(() => getIssLocation(), 5000);
 
@@ -70,19 +102,31 @@ export default function Main() {
       camera.aspect = currentRef.clientWidth / currentRef.clientHeight;
       camera.updateProjectionMatrix();
     };
-    window.addEventListener('resize', resize);
+    window.addEventListener("resize", resize);
 
     //Draco Loader
     const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath('./draco/');
+    dracoLoader.setDecoderPath("./draco/");
 
     //GLTF Loader
     const gltfLoader = new GLTFLoader();
     gltfLoader.setDRACOLoader(dracoLoader);
 
+    // stars
+    const starGeometry = new THREE.SphereGeometry(0.1, 24, 24);
+    const starMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    for (let i = 0; i < 300; i++) {
+      const star = new THREE.Mesh(starGeometry, starMaterial);
+      const [x, y, z] = Array(3)
+        .fill()
+        .map(() => THREE.MathUtils.randFloatSpread(100));
+      star.position.set(x, y, z);
+      scene.add(star);
+    }
+
     //ISS Model
     gltfLoader.load(
-      './models/iss/issDraco.gltf',
+      "./models/iss/issDraco.gltf",
       (gltf) => {
         gltf.scene.scale.set(0.01, 0.01, 0.01);
         iss.add(gltf.scene);
@@ -95,7 +139,7 @@ export default function Main() {
 
     //Earth Model
     gltfLoader.load(
-      './models/earth/earthDraco.gltf',
+      "./models/earth/earthDraco.gltf",
       (gltf) => {
         gltf.scene.scale.set(0.02, 0.02, 0.02);
         gltf.scene.rotateY(-4.7);
@@ -128,7 +172,7 @@ export default function Main() {
     animate();
 
     return () => {
-      window.removeEventListener('resize', resize);
+      window.removeEventListener("resize", resize);
       currentRef.removeChild(renderer.domElement);
       clearInterval(interval);
     };
@@ -139,22 +183,24 @@ export default function Main() {
       <div
         className="Contenedor3D"
         ref={mountRef}
-        style={{ width: '100%', height: '93vh' }}
+        style={{ width: "100%", height: "93vh" }}
       ></div>
       {!loading.loading && (
         <section
           style={{
-            position: 'absolute',
+            position: "absolute",
             top: "7vh",
+            position: "absolute",
+            top: 0,
             right: 0,
             zIndex: 1,
             marginRight: 15,
             marginTop: 15,
-            padding: '1em',
+            padding: "1em",
             width: 148,
-            color: '#000',
-            background: 'rgba(255, 255, 255, 0.7)',
-            borderRadius: '.5em',
+            color: "#000",
+            background: "rgba(255, 255, 255, 0.7)",
+            borderRadius: ".5em",
             fontSize: 12,
             lineHeight: 1.2,
           }}
@@ -181,4 +227,4 @@ export default function Main() {
       )}
     </>
   );
-};
+}
