474df91acc05604b2edca9aa1ce4d0035ffb3650
Mohamad Homsi <homsimohamad4@gmail.com>
2022-09-29 23:57:58 +0300

    ..

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import "./History.css";
 
 export default function History() {
     return (
         <div>
-            <h1>history</h1>
             <h1>history</h1>
         </div>
     );
