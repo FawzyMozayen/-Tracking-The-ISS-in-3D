16588894362e780fbbb41ed56e5f857689f92547
Mohamad Homsi <homsimohamad4@gmail.com>
2022-10-02 09:29:02 +0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import About from "./components/About/About";
 import History from "./components/History/History";
 import Spot from "./components/Spot/Spot";
+import Quiz from "./components/Quiz/Quiz";
 
 function App() {
   const loading = useLoading();
@@ -15,6 +16,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/history" element={<History />} />
         <Route path="/how-to-spot-the-iss" element={<Spot />} />
+        <Route path="/quiz" element={<Quiz />} />
       </Routes>
       <Loading visible={loading.loading} />
     </>
